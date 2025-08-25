b85f947c0cee1bc7c79032340dee63a1c261711b
PenmatsaVarma97@gmail.com <penmatsaVarma97@gmail.com>
2025-08-25 11:11:16 +0530

    Second Commit

--- app/about/page.js
+++ app/about/page.js
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 const AboutPage = () => {
     return (
         <>
             <h1>About page</h1>
             <p>We are in about page</p>
             <ul>
-                <li><a href="/about/1">About 1</a></li>
-                <li><a href="/about/2">About 2</a></li>
-                <li><a href="/about/3">About 3</a></li>
+                <li><Link href="/about/1">About 1</Link></li>
+                <li><Link href="/about/2">About 2</Link></li>
+                <li><Link href="/about/3">About 3</Link></li>
             </ul>
         </>
     )
--- app/contact/page.js
+++ app/contact/page.js
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 const ContactPage = () => {
     return (
         <>
             <h1 className="text-3xl font-bold line">Contact page</h1>
             <p>We are in contact page</p>
             <ul>
-                <li><a href="/contact/1">Contact 1</a></li>
-                <li><a href="/contact/2/title2">Contact 2</a></li>
-                <li><a href="/contact/3/title3/19082025">Contact 2</a></li>
+                <li><Link href="/contact/1">Contact 1</Link></li>
+                <li><Link href="/contact/2/title2">Contact 2</Link></li>
+                <li><Link href="/contact/3/title3/19082025">Contact 2</Link></li>
             </ul>
         </>
     )
